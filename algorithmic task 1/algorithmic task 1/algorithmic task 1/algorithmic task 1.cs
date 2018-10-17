31ea536a13bd520e3aaa49af7a5c5869d9dd2dae
Volodyaman1 <glazyrin-vladimir@rambler.ru>
2018-10-17 13:45:58 +0300

    add algorithmic task 2

--- algorithmic task 1/algorithmic task 1/algorithmic task 1/algorithmic task 1.cs
+++ algorithmic task 1/algorithmic task 1/algorithmic task 1/algorithmic task 1.cs
@@ -6,11 +6,6 @@ using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
 
-//тест с текстом весом в ~100мб проходит за ~ 8.5 секунды
-//тест с текстом весом в ~60мб проходит за ~ 5.5 секунды
-//тест с текстом весом в ~30мб проходит за ~ 2.7 секунды
-//тест с текстом весом в ~15мб проходит за ~ 1.2 секунды
-
 namespace algorithmic_task_1
 {
     public class Program
@@ -27,9 +22,10 @@ namespace algorithmic_task_1
                 s2 = i;
             }
         }
-
+        //поиск максимальной и минимальной длины путем запоминания индексов первой и последней встречи слова
         public KeyValuePair<int, int> SearchMaxAndMinLength(List<string> list, string s1, string s2)
         {
+            //объявление переменных хранения позиций встреч слов
             int s1First = -1;
             int s2First = -1;
             int s1End = -1;
@@ -40,24 +36,28 @@ namespace algorithmic_task_1
 
             s1 = s1.ToLower();
             s2 = s2.ToLower();
-
+            //перебор всех слов в списке
             for (int i = 0; i < list.Count; i++)
             {
+                //при встрече слова 1, обновляем переменные
                 if (list[i] == s1)
                 {
                     UpdateParams(ref s1First, ref s1End, i);
                 }
 
+                //при встрече слова 1, обновляем переменные
                 if (list[i] == s2)
                 {
                     UpdateParams(ref s2First, ref s2End, i);
                 }
+                //если определены значения переменных содержащих последнее значение индекса встречи 1-ого и 2-ого слова и их разница меньше минимальной длины, то обновляем значение мин длины
                 if (s1End > -1 && s2End > -1 && minLength > Math.Abs(s1End - s2End))
                 {
                     minLength = Math.Abs(s1End - s2End);
                 }
             }
-            if (s1First > -1 && s2End > -1 && s1First > -1 && s2First > -1)
+            //если переменные хранят значения индексов встречи слов 1 и 2, то определяем максимальный путь между первой встречей 1-ого слова и последней 2-ого и наоборот
+            if (s1First > -1 && s2First > -1)
             {
                 maxLength = Math.Abs(s1First - s2End);
                 if (maxLength < Math.Abs(s2First - s1End))
@@ -67,7 +67,7 @@ namespace algorithmic_task_1
             }
             return new KeyValuePair<int, int> (maxLength - 1, minLength - 1);
         }
-
+        //заполнение списка словами из строки
         public void FillList(string line, List<string> list)
         {
             String[] words = line.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
@@ -77,11 +77,11 @@ namespace algorithmic_task_1
                 list.Add(word.ToLower());
             }
         }
-
-        public List<string> LoadText()
+        //заполнение списка словами из файла
+        public List<string> LoadText(string name)
         {
             List<string> list = new List<string>();
-            StreamReader objReader = new StreamReader("../../text4.txt");
+            StreamReader objReader = new StreamReader(name);
             string sLine = "";
             
             while (sLine != null)
@@ -101,15 +101,11 @@ namespace algorithmic_task_1
             string s = "Today is very good a sunny day and tomorrow will be very good day too";
 
             Program p = new Program();
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            List<string> list = p.LoadText();
-       //     p.FillList(s, list);
+            List<string> list = new List<string>(); //p.LoadText("../../text4.txt");
+            p.FillList(s, list);
 
             KeyValuePair<int, int> answer = p.SearchMaxAndMinLength(list, "Today", "day");
-            sw.Stop();
 
-            TimeSpan ts = sw.Elapsed;
             if (answer.Value == list.Count && answer.Key == -1)
             {
                 Console.WriteLine("not found");
@@ -118,11 +114,7 @@ namespace algorithmic_task_1
             {
                 Console.WriteLine((answer.Key) + " " + (answer.Value));
             }
-
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
-                ts.Hours, ts.Minutes, ts.Seconds,
-                ts.Milliseconds / 10);
-            Console.WriteLine(elapsedTime);
+        
             Console.ReadKey();
         }
     }
---
+++ algorithmic task 2/algorithmic task 2/algorithmic_task_2.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace algorithmic_task_2
+{
+    class Program
+    {
+        //добавление имени в словарь
+        void AddNewName(string s)
+        {
+            //проверка на существование ключа первой буквы добавляемого слова
+            if (!m_notebook.ContainsKey(s[0]))
+            {
+                //создание новой пары ключ - значение первой буквы переменной s, список - пустой
+                m_notebook.Add(s[0], new List<string>());
+            }
+            //добавляем новое значение по ключу первой буквы переменной s
+            m_notebook[s[0]].Add(s);
+
+        }
+        //функция получения числа слов начинающихся со значения переменной word
+        int GetValueOfWordIncidence(string word)
+        {
+            int count = 0;
+            //проверка на существование ключа
+            if (m_notebook.ContainsKey(word[0]))
+            {
+                //получение списка строк со словами начинающихся на букву искомого слова
+                List<string> partOfNote = m_notebook[word[0]];
+                foreach (string s in partOfNote)
+                {
+                    //если слово из списка начинается с искомого слова, то увеличиваем счетчик
+                    if (s.IndexOf(word) == 0)
+                        count++;
+                }
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+            string a = "alex";
+            string a1 = "alexey";
+            string a2 = "alexandr";
+            string a3 = "aleksandr";
+            string a4 = "alekalex";
+            string a5 = "vova";
+            string a6 = "vladimir";
+            string a7 = "anton";
+            string a8 = "gosha";
+            string a9 = "igor";
+            string a10 = "sanek";
+            string a11 = "andrey";
+            string a12 = "misha";
+            string a13 = "egor";
+            string a14 = "akakii";
+            string a15 = "julia";
+
+            string b = "alex";
+
+            Program p = new Program();
+            p.AddNewName(a);
+            p.AddNewName(a1);
+            p.AddNewName(a2);
+            p.AddNewName(a3);
+            p.AddNewName(a4);
+            p.AddNewName(a5);
+            p.AddNewName(a6);
+            p.AddNewName(a7);
+            p.AddNewName(a8);
+            p.AddNewName(a9);
+            p.AddNewName(a10);
+            p.AddNewName(a11);
+            p.AddNewName(a12);
+            p.AddNewName(a13);
+            p.AddNewName(a14);
+            p.AddNewName(a15);
+
+            Console.WriteLine("Find " + b + " -> " + p.GetValueOfWordIncidence(b));
+            Console.ReadKey();
+        }
+
+        Dictionary<char, List<string>> m_notebook = new Dictionary<char, List<string>>();
+
+    }
+}
